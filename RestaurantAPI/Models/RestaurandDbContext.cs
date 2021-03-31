7d8ad1a773940a9ede9ba2836ca55aa7c732fca1
Michael Vasyukov <michael.vasukovff@gmail.com>
2021-03-31 11:04:48 +0300

    add controllers foodItem and Customer

---
+++ RestaurantAPI/Models/RestaurandDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPO.Models
+{
+    public class RestaurandDbContext : DbContext
+    {
+
+        public RestaurandDbContext(DbContextOptions<RestaurandDbContext> options) : base (options)
+        {
+             
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<OrderMaster> OrderMasters { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
